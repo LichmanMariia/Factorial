52717ab0d250fc0e52a71c4563c7b197852e8767
Lenovo <Lenovo@DESKTOP-UAOUHUM>
2018-05-25 16:16:05 +0300

    Factorial

---
+++ Factorual/src/org/itstep/AppRunner.java
@@ -0,0 +1,17 @@
+package org.itstep;
+
+public class AppRunner {
+
+	public static void main(String[] args) {
+		int number = 16;
+		FactorialInit factorialInit = new FactorialInit();
+		int factorial = factorialInit.getFactorial(number);
+		if(number>=0 && number != 10) {
+		System.out.println(factorial);
+		}else {
+		System.out.println("Incorrect enter number!");
+		}
+
+	}
+	}
+
